2957ca78dbd8d79cdad377df4083913eba6adedb
rafacampa9 <rafacampa9@gmail.com>
2023-09-20 13:37:42 +0200

    setting commit

--- CODE/WEB SCRAPING/main.py
+++ CODE/WEB SCRAPING/main.py
@@ -1,5 +1,5 @@
 from web_scraping.web_scraping import tecnocasa
-from constantes.constantes import PATH
+from constantes.constantes import PATH, CLEANUP
 import pandas as pd
 
 
@@ -10,6 +10,8 @@ if __name__=='__main__':
     viviendas = tecnocasa(province, ccaa, town)
     df = pd.DataFrame(viviendas)
     print(df)
-    df.to_json(PATH)
+    province = CLEANUP(province)
+    town = CLEANUP(town)
+    df.to_json(PATH + f'/viviendas_{province.lower()}_{town.lower()}.json')
     
     
