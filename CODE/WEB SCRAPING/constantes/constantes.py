36a50be953ff6e66e7f7a30c2a0acf3309d201a1
rafacampa9 <rafacampa9@gmail.com>
2023-09-26 20:32:16 +0200

    Ajuste final

--- CODE/WEB SCRAPING/constantes/constantes.py
+++ CODE/WEB SCRAPING/constantes/constantes.py
@@ -1,6 +1,6 @@
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 
-PATH = 'D:/Escritorio/Ubuntu/Proyecto Tecnocasa/DATA'
+PATH = 'D:/Escritorio/Proyecto Tecnocasa/DATA'
 
 def CLEANUP(word):
     if 'á' in word: word = word.replace('á', 'a')
